71e35ab66146944807f253de5be01ae0fbe93c6e
chenwa523 <48071429+chenwa523@users.noreply.github.com>
2019-04-08 12:47:52 -0400

    Update DoubleLinkedList.java
    Finished commenting the code

--- Project4/Project4Pack/DoubleLinkedList.java
+++ Project4/Project4Pack/DoubleLinkedList.java
@@ -1,17 +1,40 @@
 package Project4Pack;
 
+/***********************************************************************
+ * This is a double linked list for the the message that is going to
+ * be encrypted.
+ *
+ * @param <E> The data type for the double linked list.
+ * @author Andrew Kruse & Wayne Chen
+ * @version 4-8-19
+ **********************************************************************/
 public class DoubleLinkedList<E>  {
-	protected NodeD<E> top;      // The first NodeD<E> in the list
 
-    // This instance variable is not required, however if you
-    // find it useful, then you are welcome to use it
-	protected NodeD<E> cursor;  // The current NodeD<E> in the list
+    /*******************************************************************
+     * The first NodeD<E> in the list.
+     ******************************************************************/
+	protected NodeD<E> top;
 
+    /*******************************************************************
+     * The current NodeD in the list.
+     ******************************************************************/
+    protected NodeD<E> cursor;
+
+    /*******************************************************************
+     * This method is the constructor for the double linked list. In
+     * addition, it will set top and cursor to null;
+     ******************************************************************/
 	public DoubleLinkedList() {
 		top = null;
 		cursor = null;
 	}
 
+    /*******************************************************************
+     * This method gets the data<E> at position.
+	 *
+     * @param position The position at the given data.
+     * @return The character/data at position.
+     ******************************************************************/
 	public E get(int position) {
 		cursor = top;
 		for (int i = 0; i < position; i++)
@@ -20,6 +43,12 @@ public class DoubleLinkedList<E>  {
 
 	}
 
+    /*******************************************************************
+     * This method returns a string from the data of <E>. The works
+     * best when the data<E> is either characters or string.
+     *
+     * @return The string of the double linked list.
+     ******************************************************************/
 	public String toString() {
 		String retVal = "";
 		NodeD<E> cur = top;
@@ -31,6 +60,11 @@ public class DoubleLinkedList<E>  {
 		return retVal;
 	}
 
+    /*******************************************************************
+     * This method adds a new data to the linked list.
+     *
+     * @param data The data that is being added.
+     ******************************************************************/
 	public void addFirst(E data){
 
 		if (isEmpty()){
@@ -44,6 +78,11 @@ public class DoubleLinkedList<E>  {
 		}
 	}
 
+	/*******************************************************************
+	 * This method adds a data value at the last spot in the index.
+	 *
+	 * @param data The data that is being added to the last spot.
+	 ******************************************************************/
 	public void addLast(E data){
 
 		// Empty list
@@ -52,6 +91,7 @@ public class DoubleLinkedList<E>  {
 			addFirst(data);
 		}
 
+		// Only 1 element in the list
 		else if (size() == 1){
 
 			NodeD newNode = new NodeD(data, null, top);
@@ -71,6 +111,12 @@ public class DoubleLinkedList<E>  {
 		}
 	}
 
+	/*******************************************************************
+	 * This method adds a data value at a specific index.
+	 *
+	 * @param index The index the data is begin added at.
+	 * @param data The data that is begin added at index.
+	 ******************************************************************/
 	public void add(int index, E data){
 
 		if (index == 0){
@@ -85,6 +131,7 @@ public class DoubleLinkedList<E>  {
 			addLast(data);
 			return;
 		}
+
 
 		else if (index < 0 || index > size()){
 
@@ -109,6 +156,12 @@ public class DoubleLinkedList<E>  {
 		}
 	}
 
+	/*******************************************************************
+	 * This method adds a set of NodeD to this double linked list.
+	 *
+	 * @param index The index the set is being added to.
+	 * @param firstNode The first Node in the set of Node D.
+	 ******************************************************************/
 	public void addClip(int index, NodeD firstNode){
 
 		// Last node in the clipboard
@@ -174,6 +227,16 @@ public class DoubleLinkedList<E>  {
 		}
 	}
 
+	/*******************************************************************
+	 * The method gets a copy of a double linked list from the start
+     * index to the stop index.
+     *
+	 * @param start The start index.
+	 * @param stop The stop index.
+	 * @return A double linked list of the clip from the start index to
+     *         the stop index. The list will also include the stop
+     *         index.
+	 ******************************************************************/
 	public DoubleLinkedList getClip(int start,int stop){
 
 		DoubleLinkedList<Character> copied = new DoubleLinkedList<>();
@@ -188,17 +251,27 @@ public class DoubleLinkedList<E>  {
 			copied.addLast(startNode);
 		}
 
-		//make the last node hold the reference for null
-//		copied.findNodeD(j-1,copied).setNext(null);
-
 		return copied;
 	}
 
+    /*******************************************************************
+     * This method gets the string from start index string to the
+     * index of stop.
+     *
+     * @param start The start index.
+     * @param stop The stop index.
+     * @return The string from the start to index of stop.
+     ******************************************************************/
 	public String getStringClip(int start, int stop){
 
 		return this.toString().substring(start, stop + 1);
 	}
 
+    /*******************************************************************
+     * This method removes the first Node in the double linked list.
+     *
+     * @return The Node that was removed from the list.
+     ******************************************************************/
 	public NodeD removeFirst(){
 
 		NodeD remove = top;
@@ -209,12 +282,25 @@ public class DoubleLinkedList<E>  {
 					"list");
 		}
 
-		top = top.getNext();
-		top.setPrev(null);
+		if (top.getNext() != null){
+
+			top = top.getNext();
+			top.setPrev(null);
+		}
+
+		else{
+
+			top = null;
+		}
 
 		return remove;
 	}
 
+    /*******************************************************************
+     * This method removes the last NodeD in the double linked list.
+     *
+     * @return The NodeD that is removed.
+     ******************************************************************/
 	public NodeD removeLast(){
 
 		NodeD remove;
@@ -225,7 +311,6 @@ public class DoubleLinkedList<E>  {
 					"list");
 		}
 
-		// getting to the last spot
 		// Getting to the last spot
 		cursor = top;
 		while (cursor.getNext() != null){
@@ -244,10 +329,18 @@ public class DoubleLinkedList<E>  {
         return remove;
 	}
 
+    /*******************************************************************
+     * This method removes a node at a specific index. This will be
+     * valid as long as the index is a valid index.
+     *
+     * @param index The index that is being removed.
+     * @return The NodeD that was removed.
+     ******************************************************************/
 	public NodeD removeIndex(int index){
 
 		NodeD remove;
 
+		// Error Checks
 		if (isEmpty()){
 
 		    throw new IndexOutOfBoundsException("Empty " +
@@ -260,17 +353,19 @@ public class DoubleLinkedList<E>  {
                     "not within range");
         }
 
-
+        // Removing at the start
         if (index == 0){
 
         	remove = removeFirst();
 		}
 
+		// Removing last element
 		else if (size() - 1 == index){
 
 			remove = removeLast();
 		}
 
+		// Removing element in the middle
 		else {
 			cursor = top;
 			for (int i = 0; i < index; i++) {
@@ -288,13 +383,18 @@ public class DoubleLinkedList<E>  {
 				remove.setPrev(null);
 
 			}
-
-//        if (size() > 1)
-//        	cursor.getPrev().setNext(cursor.getNext());
 		}
         return remove;
 	}
 
+    /*******************************************************************
+     * The method removes a section from the this double linked list
+     * from start to stop. This will also include the Node at stop.
+     *
+     * @param start The start index.
+     * @param stop The stop index.
+     * @return The NodeD that is from the start index.
+     ******************************************************************/
 	public NodeD removeClip(int start, int stop){
 
 		NodeD removeStart;
@@ -303,16 +403,6 @@ public class DoubleLinkedList<E>  {
 
 		// error check
 		if (isEmpty()){
-
-			throw new IndexOutOfBoundsException();
-		}
-
-		else if (start > stop || start < 0 || start > size()){
-
-			throw new IndexOutOfBoundsException();
-		}
-
-		else if (stop < 0 || stop > size()){
 
 			throw new IndexOutOfBoundsException();
 		}
@@ -361,7 +451,8 @@ public class DoubleLinkedList<E>  {
 		}
 
 		// Index is in the middle that is not next to each other
-		else if (current.getNext() != null && removeStart.getPrev() != null){
+		else if (current.getNext() != null && removeStart.getPrev() !=
+				null){
 
 			removeStart.getPrev().setNext(current.getNext());
 			current.getNext().setPrev(removeStart.getPrev());
@@ -373,6 +464,11 @@ public class DoubleLinkedList<E>  {
 		return removeStart;
 	}
 
+    /*******************************************************************
+     * This method will determine if this double linked list is empty.
+     *
+     * @return True if this linked list is empty.
+     ******************************************************************/
 	private boolean isEmpty(){
 
 		if (top == null){
@@ -383,6 +479,11 @@ public class DoubleLinkedList<E>  {
 		return false;
 	}
 
+    /*******************************************************************
+     * The method gets the size of the this double linked list
+     *
+     * @return The size of the double linked list.
+     ******************************************************************/
 	public int size(){
 
 		cursor = top;
@@ -397,18 +498,12 @@ public class DoubleLinkedList<E>  {
 		return total;
 	}
 
-
-	private NodeD findNodeD(int index, DoubleLinkedList list){
-		NodeD data = list.top;
-
-		for (int i = 0; i < index; i++){
-			data = data.getNext();
-		}
-
-		return data;
-
-	}
-
+    /*******************************************************************
+     * This method deletes a character from the double linked list.
+     *
+     * @param data The character that is being deleted.
+     * @return The index value the character was deleted from.
+     ******************************************************************/
 	public int delete (char data) {
 
 		int index = 0;
@@ -417,14 +512,22 @@ public class DoubleLinkedList<E>  {
 		if (top == null)
 			return -1;
 
+		// deleting first data
 		if (cursor.getData().equals(data)) {
 			cursor = top.getNext();
-			if (cursor != null)
+			if (cursor != null) {
 				cursor.setPrev(null);
+				top = cursor;
+			}
 
+			else {
+
+				top = null;
+			}
 			return index;
 		}
 
+		// Deleting data that is not the first data
 		NodeD temp = top;
 		while (temp.getNext() != null) {
 
