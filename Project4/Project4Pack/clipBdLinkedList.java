eaef0523d200a83b66de2ebcb3b02a574fd38b7c
chenwa523 <48071429+chenwa523@users.noreply.github.com>
2019-04-08 13:46:47 -0400

    Update clipBdLinkedList.java
    Added comments and fixed some small errors index vs clipNum

--- Project4/Project4Pack/clipBdLinkedList.java
+++ Project4/Project4Pack/clipBdLinkedList.java
@@ -1,15 +1,39 @@
 package Project4Pack;
 
+/***********************************************************************
+ * This is the class for a single linked list for the clipboard.
+ *
+ * @author Andrew Kruse & Wayne Chen
+ * @version 4-8-19
+ **********************************************************************/
 public class clipBdLinkedList {
+
+    /*******************************************************************
+     * This is the node at the top of the list.
+     ******************************************************************/
     private NodeCB top;
+
+    /*******************************************************************
+     * This is the node at the bottom of the list.
+     ******************************************************************/
     private NodeCB tail;
 
+    /*******************************************************************
+     * This method is the constructor for the linked list.
+     ******************************************************************/
     public clipBdLinkedList() {
         tail = top = null;
     }
 
+    /*******************************************************************
+     * This method adds a new clipboard at first slot of the linked
+     * list.
+     *
+     * @param data The NodeD at the top of the linked list that is being
+     *             copied over.
+     * @param num The clipboard associated with this clipboard.
+     ******************************************************************/
     public void addFirst(NodeD<Character> data, int num){
-
 
         // Nothing is in the list yet
         if (isEmpty()){
@@ -19,6 +43,7 @@ public class clipBdLinkedList {
             tail = top;
         }
 
+        // At least one Item in the list
         else{
 
             NodeCB current = top;
@@ -29,6 +54,13 @@ public class clipBdLinkedList {
         }
     }
 
+    /*******************************************************************
+     * This method adds a new clipboard at last slot of the linked list.
+     *
+     * @param data The NodeD at the top of the linked list that is being
+     *             copied over.
+     * @param num The clipboard associated with this clipboard.
+     ******************************************************************/
     public void addLast(NodeD<Character> data, int num){
 
         // Nothing is in the list yet
@@ -46,6 +78,11 @@ public class clipBdLinkedList {
         }
     }
 
+    /*******************************************************************
+     * This method removes the first Node in the clipboard linked list.
+     *
+     * @return The first Node.
+     ******************************************************************/
     public NodeCB removeFirst(){
 
         // error if empty
@@ -68,6 +105,14 @@ public class clipBdLinkedList {
         return remove;
     }
 
+    /*******************************************************************
+     * This method searches through the clipboard list and removes
+     * the specific clipboard number.
+     *
+     * @param clipNum The clipboard number that is associated with the
+     *                NodeCB that is being removed.
+     * @return The remove NodeCB.
+     ******************************************************************/
     public NodeCB removeIndex(int clipNum){
 
         NodeCB remove;
@@ -115,8 +160,12 @@ public class clipBdLinkedList {
         return null;
     }
 
-
-    public int size(){
+    /*******************************************************************
+     * This method gets the size of the the clipboard linked list.
+     * 
+     * @return The size of the list.
+     ******************************************************************/
+    private int size(){
 
         int count = 0;
 
@@ -130,6 +179,11 @@ public class clipBdLinkedList {
         return count;
     }
 
+    /*******************************************************************
+     * This method checks if the list is empty.
+     * 
+     * @return True if the list is empty.
+     ******************************************************************/
     private boolean isEmpty(){
 
         if (size() == 0){
@@ -140,6 +194,12 @@ public class clipBdLinkedList {
         return false;
     }
 
+    /*******************************************************************
+     * This method determines if the clipboard number exist.
+     * 
+     * @param index The clipboard number that is being checked.
+     * @return True if the clipboard number exist.
+     ******************************************************************/
     public boolean indexExist(int index){
 
         NodeCB data = top;
